cacb0499d01e9e23a9d5deb325786c2304f0ca8d
Eivin Giske Skaaren <eskaaren@yahoo.no>
2015-10-20 18:11:08 +0200

    Working version

--- lib/SMS/Send/Tellustalk.pm
+++ lib/SMS/Send/Tellustalk.pm
@@ -9,42 +9,44 @@ use JSON;
 use Data::Dumper;
 
 sub new {
-    my ($class, %args) = @_;
-    die "$class needs hash_ref with _login and _password. (Optional _sender.)\n" unless $args{'_login'} && $args{'_password'};
-    my $self = bless {%args}, $class;
-    $self->{base_url}   = 'https://tellus-talk.appspot.com';
-    $self->{send_url}   = $self->{base_url} . '/send/v1';
-    $self->{_sender}    = $self->{_sender} // 'FROM SENDER'; #Add the text that describes who sent the sms if not sent as _sender to new. Max 11 chars.
-    return $self;
+	my ($class, %args) = @_;
+	die "$class needs hash_ref with _login and _password. (Optional _sender.)\n" unless $args{'_login'} && $args{'_password'};
+	my $self = bless {%args}, $class;
+	my $creds = "$self->{_login}" . ":" . "$self->{_password}";
+	chomp($self->{base64_cred} = "Basic " . encode_base64($creds));
+	$self->{base_url}    = 'https://tellus-talk.appspot.com';
+	$self->{send_url}    = $self->{base_url} . '/send/v1';
+	$self->{_sender}     = $self->{_sender} // 'FROM SENDER'; #Add the text that describes who sent the sms if not sent as _sender to new. Max 11 chars.
+	return $self;
 }
 
 sub send_sms {
-    my ($self, %args) = @_;
-    my $json_args = {
-        text                  => "$args{'text'}",
-        to                    => "$args{'to'}",
+	my ($self, %args) = @_;
+	my $json_args = {
+		text                  => "$args{'text'}",
+		to                    => "sms:$args{'to'}",
 		sms_originator_source => "text",
 		sms_originator_text   => "$self->{_sender}"
-    };
-    my $response = _post($self->{send_url}, $json_args);
-
-    if ($response->{status} eq "200") {
+	};
+	my $response = _post($self, to_json($json_args));
+	if ($response->{status} eq "200") {
 		return 1;
 	}
-    return 0;
+	return 0;
 }
 
 sub _post {
-    my ($url, $json_args) = @_;
-    return HTTP::Tiny->new->post(
-        $url => {
-            content => to_json($json_args),
-            headers => {
-                "Content-Type"  => "application/json",
-				"Authorization" => "Basic " . encode_base64("$self->{_login}:$self->{_password}"),
-            },
-        },
-    );
+	my ($self, $content) = @_;
+	return HTTP::Tiny->new->post(
+		$self->{send_url} => {
+			content => $content,
+			headers => {
+				"accept"        => "application/json",
+				"content-type"   => "application/json",
+				"authorization"  => "$self->{base64_cred}"
+			}
+		}
+	);
 }
 
 1;
@@ -61,21 +63,21 @@ SMS::Send::Tellustalk - SMS::Send driver to send messages via Tellustalk Rest AP
 
   # Create a sender
   my $sender = SMS::Send->new('Tellustalk',
-        _login    => 'username',
-        _password => 'password',
-        _sender   => 'FROM ME', # Optional, max 11 chars.
+		_login    => 'username',
+		_password => 'password',
+		_sender   => 'FROM ME', # Optional, max 11 chars.
   );
 
   # Send a message
   my $sent = $sender->send_sms(
-        text => 'This is a test message',
-        to   => '+4612345678',
+		text => 'This is a test message',
+		to   => '+4612345678',
   );
 
   if ( $sent ) {
-        print "Message sent ok\n";
+		print "Message sent ok\n";
   } else {
-        print "Failed to send message\n";
+		print "Failed to send message\n";
   }
 
 
